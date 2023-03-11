f5fe7123d3b3e3930bb384613175f25e76785dd6
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-03-11 18:41:38 +0300

    Fixed test issues

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -19,8 +19,9 @@ class ProjectsComparator implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        if (o1.length() == o2.length())
-            return 0;
+        if (o1.length() == o2.length()) {
+            return -o1.compareTo(o2);
+        }
         return o1.length() > o2.length() ? -1 : 1;
     }
 }
